2f1970da44e6a04be4ab4bb9679c6167d97f0a39
f24ba071 <f24ba071@chuo.ac.jp>
2025-07-16 14:51:27 +0900

    snsなど

---
+++ script.js
@@ -0,0 +1,42 @@
+// search
+function opensearch() {
+    document.getElementById('search').classList.add('open');
+}
+
+function closesearch() {
+    document.getElementById('search').classList.remove('open');
+}
+// post
+function openpost() {
+    document.getElementById('post').classList.add('open');
+}
+
+function closepost() {
+    document.getElementById('post').classList.remove('open');
+}
+// favo
+function openfavo() {
+    document.getElementById('favo').classList.add('open');
+}
+
+function closefavo() {
+    document.getElementById('favo').classList.remove('open');
+}
+// クーポン、キャンペーンページ-----------------------------------
+// ---------------------------------------------------------------------------------------
+// kupon
+function openkupon() {
+    document.getElementById('kupon').classList.add('open');
+}
+
+function closekupon() {
+    document.getElementById('kupon').classList.remove('open');
+}
+// can
+function opencan() {
+    document.getElementById('can').classList.add('open');
+}
+
+function closecan() {
+    document.getElementById('can').classList.remove('open');
+}
